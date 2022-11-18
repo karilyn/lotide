d00a740a25f24f79779081f6ca209897dad5a121
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-18 21:15:18 +0000

    adding tail to lotide

--- tail.js
+++ tail.js
@@ -18,9 +18,14 @@ const tail = function(array) {
   return tail;
 };
 
-const result = tail(["Hello"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
