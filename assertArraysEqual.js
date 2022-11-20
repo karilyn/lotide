223a2903f53f9a541e5b69fc3d1fc8ba781f15b3
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-20 19:12:59 +0000

    undoing an accidental deletion

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -21,5 +21,37 @@ const assertArraysEqual = function(array1, array2) {
   console.log(`✅Assertion Passed: ${array1} === ${array2}`);
 }
 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // arrays are not equal length
+    return false;
+  }
+
+  //if one of the arrays has no value
+  if (array1 === null || array2 === null) {
+
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    // if the items are not equal
+    if (array1[i] !== array2[i]) {
+     return false;
+    }
+  }
+
+  return true;
+}
+
+
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
