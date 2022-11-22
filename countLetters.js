2de3a318561b2aa4dce615694c8cfdb09a67d7ba
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-22 22:00:52 +0000

    function working, passing tests

--- countLetters.js
+++ countLetters.js
@@ -1,6 +1,28 @@
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`❌Assertion Failed ${actual} !== ${expected}`);
   }
+};
+// str is the string we need to look at
+const countLetters = function(str) {
+  // we want the result to be an object
+  const results = {};
+  //we want to remove spaces because they don't need to be counted
+  str = str.replace(' ', '');
+  for (const letter of str) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+// test code
+const result1 = countLetters("lighthouse in the house");
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["g"], 1);
