4cdc75327d86b347a60ecd87dd7427851d9df3e8
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-23 18:38:56 +0000

    modified after some copy and pasting

--- letterPositions.js
+++ letterPositions.js
@@ -6,11 +6,14 @@ const letterPositions = function(sentence) {
   sentence = sentence.replace(/\s+/g, '');
 
   for (let i = 0; i < sentence.length; i++) {
+    // key is the character at that index in the string
     let key = sentence[i];
+    // check to see if the key character is an array or not
     if (!Array.isArray(results[key])){
-      // if it's not already an array, return a new empty array
+      // if it's not already an array, initialize a new empty array
       results[key] = [];
     }
+    //push the index number as a value into the key
     results[key].push(i);
   }
 
