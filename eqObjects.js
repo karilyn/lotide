c0493bab1c540c3e115dc1b1e04a4df6f396b633
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-23 19:43:09 +0000

    muddling though errors

--- eqObjects.js
+++ eqObjects.js
@@ -7,12 +7,55 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
+  const keyResult1 = Object.keys(object1);
 
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const key of keyResult1) {
+    if (object1[key] !== object2[key]); {
+      return false
+    }
+  } return true
+ }
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // arrays are not equal length
+    return false;
+  }
+
+  //if one of the arrays has no value
+  if (array1 === null || array2 === null) {
+
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    // if the items are not equal
+    if (array1[i] !== array2[i]) {
+     return false;
+    }
+  }
+
+  return true;
 }
+
+
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1"};
-console.log(assertEqual(ab, ba)) // this should evaluate to true because order isn't important
+// assertEqual(eqObjects(ab, ba), true)
+eqObjects(ab, ba); // true because order isn't important
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(assertEqual(ab, abc));
+// assertEqual(eqObjects(ab, abc), false);
+eqObjects(ab, abc);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
