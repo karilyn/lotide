6752e549f84c9731110fa843b9fe1d21b569fdcb
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-23 21:10:53 +0000

    changed variable names in eqObjects for readability

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,24 +1,23 @@
 const assertObjectsEqual = function(actual, expected) {
   // use the util library's inspect function so that assertion messages don't pring out [object Object]
   const inspect = require('util').inspect;
-  //
+  if (eqObjects(actual, expected) === true) {
+    console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`❌Assertion Failed ${insepct(actual)} !== ${inspect(expected)}`);
+  }
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
-  }
-};
-
 const eqObjects = function(object1, object2) {
-  const array1 = Object.keys(object1);
-  const array2 = Object.keys(object2);
-  if (array1.length !== array2.length) {
+  // extract the keys from each object
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  // check to see if the key lengths match
+  if (object1Keys.length !== object2Keys.length) {
     return false;
   }
-  for (let key of array1) {
+  // iterate over the keys
+  for (let key of object1Keys) {
     // use eqArrays function if values are arrays
     if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
@@ -30,3 +29,44 @@ const eqObjects = function(object1, object2) {
   }
   return true;
  }
+
+
+ const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // arrays are not equal length
+    return false;
+  }
+
+  //if one of the arrays has no value
+  if (array1 === null || array2 === null) {
+
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    // if the items are not equal
+    if (array1[i] !== array2[i]) {
+     return false;
+    }
+  }
+
+  return true;
+}
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1"};
+assertObjectsEqual(eqObjects(ab, ba), true)
+// console.log(eqObjects(ab, ba)); // true because order isn't important
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(eqObjects(ab, abc), false);
+// console.log(eqObjects(ab, abc));
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(eqObjects(cd, dc), true); // => true
+// console.log(eqObjects(cd, dc));
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(eqObjects(cd, cd2), false); // => false
+// console.log(eqObjects(cd, cd2));
