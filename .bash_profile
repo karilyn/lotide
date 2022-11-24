4818caa461d85836f5f2dbd2412553b1813dfb56
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-24 21:23:46 +0000

    got function working as expected and passing assertArraysEqual tests

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+  if [ -f "/usr/local/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+    __GIT_PROMPT_DIR="/usr/local/opt/bash-git-prompt/share"
+    source "/usr/local/opt/bash-git-prompt/share/gitprompt.sh"
+  fi
--- takeUntil.js
+++ takeUntil.js
@@ -1,18 +1,60 @@
-// The function will return a "slice of the array with elements taken from the beginning."
-
-const takeUntil = function(array, callback) {
-// the callback is its own function with one value: the item in the array
-// the result will be an array
-  return array
-}
-
-callback function(x) {
-  for (let i = 0; i < array.length; i++) {
-    if (i === x) {
-      return true
-    }
+// adding in assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
   }
 };
+// adding in assertArraysEqual function for testing
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
+// adding eqArrays function so that assertArraysEqual will work
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // arrays are not equal length
+    return false;
+  }
+  //if one of the arrays has no value
+  if (array1 === null || array2 === null) {
+
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    // if the items are not equal
+    if (array1[i] !== array2[i]) {
+     return false;
+    }
+  }
+  return true;
+}
+
+
+
+// The function will return a "slice of the array with elements taken from the beginning."
+const takeUntil = function(array, callback) {
+// the callback is its own function with one value: the item in the array
+// the result will be a new array
+  let newArray = [];
+  // iterate though each index in the array
+  for (let i = 0; i < array.length; i++) {
+    // look for the index where the callback function hits the stopping point
+    if (callback(array[i])) {
+      // if this condition is met, the return stops the function
+      break;
+    }
+    // add the array items to the new array until the condition is met
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
 
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
@@ -24,3 +66,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(["1", "2", "5", "7", "2"], ["1", "2", "5", "7", "2"]);
+assertArraysEqual(["I've", "been", "to", "Hollywood",], ["I've", "been", "to", "Hollywood"]);
