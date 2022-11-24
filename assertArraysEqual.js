78dd8226c65db86495b640df0b2ac3683ef8d1f1
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-24 06:38:01 +0000

    changed testing on to remove an extra value

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected) === true) {
     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
@@ -29,5 +37,5 @@ const eqArrays = function(array1, array2) {
 }
 
 
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
