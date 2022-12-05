31e719ae5f742d2e5e4d7a1106a107e1e949446a
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-05 23:12:14 +0000

    fresh commit after linting

--- eqArrays.js
+++ eqArrays.js
@@ -15,11 +15,11 @@ const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     // if the items are not equal
     if (array1[i] !== array2[i]) {
-     return false;
+      return false;
     }
   }
 
   return true;
-}
+};
 
 module.exports = eqArrays;
