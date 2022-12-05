2c0edd5f466e4527ab3f583b7826a93bc9bb2d22
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-05 22:33:56 +0000

    moving tests to separate test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("identical string", "identical string");
+assertEqual("close", "Close");
+assertEqual(22, 22);
+assertEqual("22", 22);
+assertEqual(1, 2);
