d578cda78304a4588d1541a35681894a2cf66cd1
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-05 22:58:21 +0000

    refactored to remove tests, require assertEqual and export tail function

--- tail.js
+++ tail.js
@@ -1,10 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
+
 
 const tail = function(array) {
   if (array.length === 0 || array.length === 1) {
@@ -19,13 +15,5 @@ const tail = function(array) {
 };
 
 
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
