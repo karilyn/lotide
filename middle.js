3d031806e377cd544352af00939869057e3a9619
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-05 23:32:36 +0000

    require assertArraysEqual, removed tests, linted

--- middle.js
+++ middle.js
@@ -1,47 +1,6 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    // arrays are not equal length
-    return false;
-  }
+const assertArraysEqual = require('./assertArraysEqual');
 
-  //if one of the arrays has no value
-  if (array1 === null || array2 === null) {
 
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    // if the items are not equal
-    if (array1[i] !== array2[i]) {
-     return false;
-    }
-  }
-
-  return true;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  // arrays are not equal length
-  if (array1.length !== array2.length) {
-    console.log(`❌Assertion Failed ${array1} !== ${array2}`);
-    return;
-  }
-
-  //if one of the arrays has no value
-  if (array1 === null || array2 === null) {
-    console.log(`❌Assertion Failed ${array1} !== ${array2}`);
-    return;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    // if the items are not equal
-    if (array1[i] !== array2[i]) {
-      console.log(`❌Assertion Failed ${array1} !== ${array2}`);
-      return;
-    }
-  }
-  console.log(`✅Assertion Passed: ${array1} === ${array2}`);
-}
 // The middle function should return an array with only the middle element(s) of the provided array.
 const middle = function(array) {
   let a = [];
@@ -59,11 +18,7 @@ const middle = function(array) {
     a.push(array[startIndex + 1]);
   }
   return a;
-}
+};
 
-assertArraysEqual(middle([1]), ([]))
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), ([3]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), ([4, 5]));
-assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]));
+module.exports = middle;
 
