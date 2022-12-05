328ccbfecaab259fdd45d390b88ad0d669b9fa31
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-05 22:32:06 +0000

    removed tests from head.js

--- head.js
+++ head.js
@@ -4,13 +4,5 @@ const head = function(array) {
   return array[0];
 };
 
-
-
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
-
-module.exports = assertEqual;
 module.exports = head;
+
