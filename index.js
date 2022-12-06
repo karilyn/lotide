096a25ebd36e5a54136204ac502b0aed78f7515c
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-06 05:10:07 +0000

    adding code to export functions as part of an object

---
+++ index.js
@@ -0,0 +1,17 @@
+// this code will export some of the functions as part of an object
+
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  
+};
