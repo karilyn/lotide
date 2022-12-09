e00bff98efe295890c951c6c358037344b3abdbd
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-09 21:41:48 +0000

    removing tests and assertions

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
-  }
-};
 
 // return the first key which contains the given value
 // if no given value is found, return undefined
@@ -17,11 +10,7 @@ const findKeyByValue = function(objectToSearch, value) {
   }
 }
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
+
+
