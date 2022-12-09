ee54443442a281b8956f82c8cf2ce4aba16103fd
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-09 22:04:55 +0000

    changed assertion to deepEqual from strictEqual

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,17 +4,18 @@ const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
   it("returns true for ([1, 2, 3], [1, 2, 3])", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
   it("returns false for ([1, 2, 3], [3, 2, 1])", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
   });
   it("returns true for (['1', '2', '3'], ['1', '2', '3'])", () => {
-    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+    assert.deepEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
   });
   it("returns true for (['1', '2', '3'], ['1', '2', 3])", () => {
-    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+    assert.deepEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
   });
+
 
 });
 
