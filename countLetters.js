0e0750a29913b4e957fe563a6d024c7bdf9dff9c
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-09 22:39:45 +0000

    refactored countLetters to remove assertions, export module, remove test and lint

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,5 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
-  }
-};
+
 // str is the string we need to look at
 const countLetters = function(str) {
   // we want the result to be an object
@@ -21,9 +15,6 @@ const countLetters = function(str) {
   }
   return results;
 };
-// console.log(countLetters("lighthouse in the house"));
-// test code
-const result1 = countLetters("lighthouse in the house");
-assertEqual(result1["l"], 1);
-assertEqual(result1["i"], 2);
-assertEqual(result1["g"], 1);
+
+module.exports = countLetters;
+
