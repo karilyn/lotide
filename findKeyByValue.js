4970daa4acd4bb40c39749748d5021705d677580
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-09 21:46:00 +0000

    linted

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,14 +1,15 @@
-
 // return the first key which contains the given value
 // if no given value is found, return undefined
 const findKeyByValue = function(objectToSearch, value) {
+  // extract the keys from objectToSearch
   const keyResult = Object.keys(objectToSearch);
+  // look through all the keys
   for (const key of keyResult) {
     if (objectToSearch[key] === value) {
       return key;
     }
   }
-}
+};
 
 
 module.exports = findKeyByValue;
