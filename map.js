9b572c5a58a1a35d5617e4fdb877fefbef102ca4
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-09 23:13:23 +0000

    linted

--- map.js
+++ map.js
@@ -4,10 +4,10 @@
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-   results.push(callback(item));
+    results.push(callback(item));
   }
   return results;
-}
+};
 
 module.exports = map;
 
