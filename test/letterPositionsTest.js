1e90f24051f50fba744cb06a51f8314714a74e79
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-09 21:43:06 +0000

    removed should from test statements

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -3,16 +3,16 @@ const letterPositions = require('../letterPositions');
 
 
 describe("#letterPositions", () => {
-  it("should return [1] for for 'e' in 'hello'", () => {
+  it("returns [1] for for 'e' in 'hello'", () => {
     assert.deepEqual(letterPositions("hello").e, [1]);
   });
-  it("should return [2, 3] for for 'l' in 'hello'", () => {
+  it("returns [2, 3] for for 'l' in 'hello'", () => {
     assert.deepEqual(letterPositions("hello").l, [2, 3]);
   });
-  it("should return [0] for for 'h' in 'hello'", () => {
+  it("returns [0] for for 'h' in 'hello'", () => {
     assert.deepEqual(letterPositions("hello").h, [0]);
   });
-  it("should return [0, 10] for for 'l' in 'lighthouse labs'", () => {
+  it("returns [0, 10] for for 'l' in 'lighthouse labs'", () => {
     assert.deepEqual(letterPositions("lighthouse labs").l, [0, 10]);
   });
 
