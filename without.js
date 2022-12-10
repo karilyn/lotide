cfd93eb8b40f97592886d2604ff8b4ecc5baef83
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-10 17:46:23 +0000

    refactored to remove assertions and tests, and exported the without function

--- without.js
+++ without.js
@@ -1,39 +1,11 @@
-const assertArraysEqual = function(array1, array2) {
-  // arrays are not equal length
-  if (array1.length !== array2.length) {
-    console.log(`❌Assertion Failed ${array1} !== ${array2}`);
-    return;
-  }
-
-  //if one of the arrays has no value
-  if (array1 === null || array2 === null) {
-    console.log(`❌Assertion Failed ${array1} !== ${array2}`);
-    return;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    // if the items are not equal
-    if (array1[i] !== array2[i]) {
-      console.log(`❌Assertion Failed ${array1} !== ${array2}`);
-      return;
-    }
-  }
-  console.log(`✅Assertion Passed: ${array1} === ${array2}`);
-}
 // return a subset of a given array, removing unwanted elements
 let without = function(source, itemsToRemove) {
-  let newArray = source.filter(function(value){
+  let newArray = source.filter(function(value) {
     // return false if it is in itemsToRemove
-
     return !itemsToRemove.includes(value);
   });
 
   return newArray;
-}
+}9
 
-
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
