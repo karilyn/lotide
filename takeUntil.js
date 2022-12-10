177b4356368f7333e1274a8162604eb014a5606b
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-10 17:30:55 +0000

    refactored takeUntil to remove assertions and tests, exported the module

--- takeUntil.js
+++ takeUntil.js
@@ -1,43 +1,4 @@
-// adding in assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
-  }
-};
-// adding in assertArraysEqual function for testing
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed ${actual} !== ${expected}`);
-  }
-};
-
-// adding eqArrays function so that assertArraysEqual will work
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    // arrays are not equal length
-    return false;
-  }
-  //if one of the arrays has no value
-  if (array1 === null || array2 === null) {
-
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    // if the items are not equal
-    if (array1[i] !== array2[i]) {
-     return false;
-    }
-  }
-  return true;
-}
-
-
-
-// The function will return a "slice of the array with elements taken from the beginning."
+// return a slice of the array with elements taken from the beginning.
 const takeUntil = function(array, callback) {
 // the callback is its own function with one value: the item in the array
 // the result will be a new array
@@ -53,19 +14,7 @@ const takeUntil = function(array, callback) {
     newArray.push(array[i]);
   }
   return newArray;
-}
+};
 
 
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(["1", "2", "5", "7", "2"], ["1", "2", "5", "7", "2"]);
-assertArraysEqual(["I've", "been", "to", "Hollywood",], ["I've", "been", "to", "Hollywood"]);
+module.exports = takeUntil;
